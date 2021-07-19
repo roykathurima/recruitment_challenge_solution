daf08b8501ff14a84ad97e4c0e68945b92ec5d4a
kathurimaroy <kathurimaroy@gmail.com>
2021-07-19 18:44:01 +0300

    Removed the test Routes. Added the node-postgres lib to help interface with postgres. Setup the local and production configurations for the database

---
+++ pizzeria/backend/config/db_config.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+    user: 'content',
+    host: 'localhost',
+    database: 'roy_test',
+    password: null,
+    port: 5432,
+}
+module.exports = dbConfig;
--- pizzeria/backend/server.js
+++ pizzeria/backend/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 // The lib helps deal with the browser's cross-orgin resource-sharing policy
 const cors = require('cors');
 
+// node-postgres lib to interface with the postgres database
+const { Pool } = require('pg')
+
+// Local Test Database Config
+const dbConfig = require('./config/db_config');
+
 const app = express();
 
 // We'll allow all origins
@@ -16,13 +22,20 @@ app.use((req, res, next)=>{
     next()
 });
 
-// Route to test if we are online
-app.get('/', (req, res)=>{
-    res.send({'error':'0', 'message':'The Server is online...'});
-});
-app.get('/angular_test/', (req, res)=>{
-    res.send({'error':'0', 'message':'The Rad app'});
-});
+// Connection credentials for the database
+let conn_credentials = dbConfig
+
+// Heroku already has a NODE_ENV environment var set to production
+// We use it know if we are developing locally or we have deployed to heroku
+if(process.env.NODE_ENV == 'production'){
+    // the DATABASE_URL is also exposed as an environmnent var in Heroku
+    // We use it to connect to the db added in heroku
+    conn_credentials = {connectionString: process.env.DATABASE_URL}
+}
+// I hear connection pools are better than clients :)
+const pool = new Pool({
+    ...conn_credentials
+})
 
 
 const PORT = process.env.PORT || 3000
