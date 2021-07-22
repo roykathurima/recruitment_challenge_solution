e6c973ab6e237cb213affc83b47fc5f67105ba89
kathurimaroy <kathurimaroy@gmail.com>
2021-07-22 13:09:57 +0300

    Added an order service and added methods for getting the orders

---
+++ pizzeria/src/app/services/order.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// Import the Environment
+import { environment } from 'src/environments/environment';
+
+
+// The Order Shape
+export interface Order {
+  id: number,
+  customer_name: string,
+  gross_total: number,
+  order_date: Date,
+  order_items: Array<OrderItem>
+}
+
+// The shape of the order Item
+export interface OrderItem{
+  quantity: number,
+  pizza_name: string,
+  unit_price: string,
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  apiUrl = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  // Fetch all the orders -> For the Admin
+  fetchOrders(): Promise<Array<Order>>{
+    return new Promise((resolve, reject)=>{
+      const url = this.apiUrl + "/get-orders/";
+      this.http.get(url).toPromise()
+      .then(orders=>resolve(orders as Array<Order>))
+      .catch(err=>reject(err));
+    })
+  }
+
+  // Fetch a single order given the ID
+  fetchOrderWithId(id: number): Promise<Order>{
+    return new Promise((resolve, reject)=>{
+      const url = this.apiUrl + '/get-order/';
+      this.http.post(url, {id}).toPromise()
+      .then(order=>resolve(order as Order))
+      .catch(err=>reject(err));
+    })
+  }
+}
