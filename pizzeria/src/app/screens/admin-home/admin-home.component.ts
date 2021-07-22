ef8ab7f2074f0891776b77dbb4530ee53d9eb770
kathurimaroy <kathurimaroy@gmail.com>
2021-07-22 06:02:14 +0300

    Added Admin Home functionality to fetch and filter pizzas

--- pizzeria/src/app/screens/admin-home/admin-home.component.ts
+++ pizzeria/src/app/screens/admin-home/admin-home.component.ts
@@ -1,32 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
+// Get the Pizza Service Right Here
+import { PizzaService, Pizza } from 'src/app/services/pizza.service';
 
-interface Transaction {
-  product: string;
-  price: number;
-}
+// The Error Component
+import { ErrorMessageComponent } from 'src/app/components/error-message/error-message.component';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
-  styleUrls: ['./admin-home.component.css']
+  styleUrls: ['./admin-home.component.css'],
+  providers: [PizzaService],
 })
 export class AdminHomeComponent implements OnInit {
   isloading = false;
 
-  constructor() { }
+  search_keyword = '';
+  // For the Table
+  displayedColumns: string[] = ['name', "price"];
+  pizzas: Pizza[] = [];
+  // A duplicate Array to restore pizzas after search mutations
+  pizzas_cp: Pizza[] = [];
+
+  constructor(
+    private pservice: PizzaService,
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
+    this.isloading = true;
     // Fetch the Pizzas Right Here
-    // Remember that there is a search feature...
+    this.pservice.getPizzas()
+    .then(pizza_array=>{
+      this.isloading = false;
+      this.pizzas = pizza_array
+      this.pizzas_cp = pizza_array
+    })
+    .catch(err=>{
+      this.isloading = false;
+      this.dialog.open(ErrorMessageComponent, {data: {message:err.message?err.message:'Failed to fetch Pizzas'}})
+    })
   }
 
-  displayedColumns: string[] = ['product', "price"];
-  transactions: Transaction[] = [
-    {product: 'Pepperoni Cheese Pizza', price: 4},
-    {product: 'Beef Pizza', price: 5},
-    {product: 'Chicken Pizza', price: 2},
-    {product: 'Sunscreen', price: 4},
-    {product: 'Berbecue Pizza', price: 25},
-  ];
+  searchPizzas(){
+    const keyword = this.search_keyword.trim().toLowerCase();
+    if(keyword == ""){
+      this.pizzas = this.pizzas_cp
+    }else{
+      this.pizzas = this.pizzas_cp.filter(pizza=>pizza.name.toLowerCase().trim().includes(keyword));
+    }
+  }
 }
